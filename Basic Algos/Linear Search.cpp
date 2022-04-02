ae687af67db6042636eb33900cc436d7def42518
Saubhagya Chopra <75625298+saubhagya0111@users.noreply.github.com>
2022-04-02 20:36:44 +0530

    Create Linear Search.cpp

---
+++ Basic Algos/Linear Search.cpp
@@ -0,0 +1,11 @@
+int linear_search(int arr[], int n, int key)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
